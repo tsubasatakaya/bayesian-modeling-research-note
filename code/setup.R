7410d383e96f41bf7561a330aa738652f51b8c4c
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2025-05-14 18:42:15 +0200

    add R scripts

---
+++ code/setup.R
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------------------------
+# Install packages and specify paths
+#-------------------------------------------------------------------------------
+packages <-  c(
+  "tidyverse", 
+  "ggplot2",
+  "cmdstanr"
+)
+
+package.check <- lapply(
+  packages,
+  FUN <-  function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+data_path <- "data"
+output_path <- "output"
+stan_path <- "stan_files"
+
+if (!dir.exists(output_path)) {
+  dir.create(output_path, recursive = TRUE)
+}
+
+if (!dir.exists(stan_path)) {
+  dir.create(stan_path, recursive = TRUE)
+}
