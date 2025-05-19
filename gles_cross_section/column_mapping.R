2caf1166b6bf78216fcf4de6718baeb677b74371
T.Takaya <117486701+tsubasatakaya@users.noreply.github.com>
2025-05-19 15:58:07 +0200

    modify column mapping

--- gles_cross_section/column_mapping.R
+++ gles_cross_section/column_mapping.R
@@ -1,25 +1,3 @@
-mapping_2009 <- c(
-  "year" = "year",
-  "ID" = "vw_lfdn",
-  "state" = "land",
-  "east_west_1" = "ostwest", # all of Berlin = East
-  "east_west_2" = "ostwest2",  # classification of Berlin
-  "gender" = "q1",
-  "age" = "q1a",
-  "satis_demo" = "q5",
-  "vote_int_party" = "q11a",
-  "person_econ_current" = "q18",
-  "position_socioecon" = "q63",
-  "position_liber_autho" = "q64",
-  "party_identification" = "q139a",
-  "year_birth" = "d201b",
-  "school_certificate" = "d206",
-  "employment" = "d209",
-  "unemp_past" = "d212",
-  "fear_job_loss" = "d226",
-  "income" = "d270b"
-)
-
 mapping_2013 <- c(
   "year" = "year",
   "ID" = "lfdn",
@@ -28,7 +6,8 @@ mapping_2013 <- c(
   "gender" = "q1",
   "year_birth" = "q2c",
   "satis_demo" = "q6",
-  "vote_int_party" = "q11aa",
+  "vote_int_first" = "q11aa",
+  "vote_int_second" = "q11ba",
   "person_econ_current" = "q17",
   # "position_socioecon" = "q67",
   # "position_liber_autho" = "q68",
@@ -49,7 +28,8 @@ mapping_2017 <- c(
   "gender" = "q1",
   "year_birth" = "q2a",
   "satis_demo" = "q6",
-  "vote_int_party" = "q11aa",
+  "vote_int_first" = "q11aa",
+  "vote_int_second" = "q11ba",
   "person_econ_current" = "q15",
   # "position_socioecon" = "q59",
   # "position_liber_autho" = "q60",
@@ -69,7 +49,8 @@ mapping_2021 <- c(
   "gender" = "d1",
   "year_birth" = "d2a",
   "satis_demo" = "q4",
-  "vote_int_party" = "q8aa",
+  "vote_int_first" = "q8aa",
+  "vote_int_second" = "q8ba",
   "person_econ_current" = "q13",
   "party_identification" = "q75a",
   "school_certificate" = "d7",
